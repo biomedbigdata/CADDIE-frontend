f83f67d829aa7fef87f548d37d01f65abfb5b71a
Michael Hartung <michi@Michaels-MacBook-Pro.local>
2021-12-13 14:39:25 +0100

    data privacy disclaimer

--- src/app/components/cookie-disclaimer/cookie-disclaimer.component.ts
+++ src/app/components/cookie-disclaimer/cookie-disclaimer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cookie-disclaimer',
@@ -9,7 +9,22 @@ export class CookieDisclaimerComponent implements OnInit {
 
   constructor() { }
 
+  public accepted = false;
+  private storageKey = `caddie-data-disclaimer-${window.location.host}`;
+
   ngOnInit(): void {
+    if (localStorage.getItem(this.storageKey) === 'true') {
+      this.accepted = true;
+    }
+  }
+
+  public accept() {
+    localStorage.setItem(this.storageKey, 'true');
+    this.accepted = true;
+  }
+
+  public informationLink() {
+    window.location.href = "/about#data-privacy-disclaimer"
   }
 
 }

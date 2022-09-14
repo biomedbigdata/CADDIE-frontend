e38396bcf70a780bcb68d871f4d4cc698c6f205b
Hartung, Michael <michael.hartung@uni-hamburg.de>
2022-09-14 11:39:54 +0200

    CADDIE v1.1

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,2 @@
-docker build -t gitlab.lrz.de:5005/caddie/frontend:prod -f ./Dockerfile.uhh.prod .
+docker build -t gitlab.lrz.de:5005/caddie/frontend:prod -f ./Dockerfile.prod .
 docker push gitlab.lrz.de:5005/caddie/frontend:prod
